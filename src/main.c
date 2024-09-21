45064680492b5f5fe17e13b01070516a2ecca8ec
mhoikka <56174759+mhoikka@users.noreply.github.com>
2024-09-20 23:26:39 -0500

    Update main.c

--- src/main.c
+++ src/main.c
@@ -137,7 +137,7 @@ void  UART_Settings_Init(){
   USART_InitStruct.USART_WordLength = USART_WordLength_8b;
   USART_InitStruct.USART_StopBits = USART_StopBits_1;
   USART_InitStruct.USART_Parity = USART_Parity_No;
-  USART_InitStruct.USART_Mode = USART_Mode_Tx;
+  USART_InitStruct.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;
   USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
   USART_Init(USART1, &USART_InitStruct);
   //No synchronous mode yet
