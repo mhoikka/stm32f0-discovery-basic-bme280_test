61052df74c2df8c6e31b00682deafa1e3e1b8aac
mhoikka <mhoikka@asu.edu>
2024-09-24 09:20:08 -0500

    Careless

--- src/main.c
+++ src/main.c
@@ -43,9 +43,8 @@ int main(void)
 
   System_Clock_Init();
   I2C_Settings_Init();
-  /*
   UART_Settings_Init();
-
+  /*
   Delay(1000);
   send_string("Start");
   struct bme280_dev bme280_initparam;
