d05b26036304273698a08495c3d4a4136d3a7110
mhoikka <mhoikka@asu.edu>
2024-10-06 14:28:44 -0500

    SPI test was not in fact working

--- src/main.c
+++ src/main.c
@@ -243,11 +243,11 @@ void test_nrf24_connection() {
 
     
     // Example to power up and set to RX mode
-    nrf24_write_register(NRF24L01_CONFIG, 0x02); // PWR_UP=1
-    nrf24_write_register(NRF24L01_CONFIG, 0x03); // PWR_UP=1 and PRIM_RX=1
-    Delay(1);
-    set_nrf24_SPI_CE(1);
-    Delay(1);
+    //nrf24_write_register(NRF24L01_CONFIG, 0x02); // PWR_UP=1
+    //nrf24_write_register(NRF24L01_CONFIG, 0x03); // PWR_UP=1 and PRIM_RX=1
+    //Delay(1);
+    //set_nrf24_SPI_CE(1);
+    //Delay(1);
 
     // Optionally, you can read back the CONFIG register to verify
     uint8_t configValue = nrf24_read_register(NRF24L01_CONFIG);
