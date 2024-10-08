9a5a437c876a9b71b7a3cf3dfe35529849c4a96d
mhoikka <mhoikka@asu.edu>
2024-10-07 20:13:21 -0500

    Please tell me this was not the issue

--- src/main.c
+++ src/main.c
@@ -260,8 +260,9 @@ void test_nrf24_connection() {
     set_nrf24_SPI_CSN(1);
     set_nrf24_SPI_CE(0);
 
-    uint8_t configValue = nrf24_read_register(NRF24L01_ENAA);
-    //nrf24_write_register(NRF24L01_CONFIG, 0x03); // PWR_UP=1 and PRIM_RX=1
+    uint8_t configValue = nrf24_read_register(NRF24L01_CONFIG);
+    nrf24_write_register(NRF24L01_CONFIG, 0x0B); // PWR_UP=1, PRIM_RX=1, CRC_EN=1
+    uint8_t configValue = nrf24_read_register(NRF24L01_CONFIG);
     //set_nrf24_SPI_CE(1); //enables chip to receive data
     Delay(2);
     uint8_t configValue2 = nrf24_read_register(NRF24L01_CONFIG);
