f184724e0b89b78d4bf9a9abf9023781b6bdb157
mhoikka <mhoikka@asu.edu>
2024-10-09 20:48:19 -0500

    Cleaning up code

---
+++ src/Peripheral_Init.h
@@ -0,0 +1,22 @@
+/**
+  ******************************************************************************
+  * @file    Peripheral_Init.h
+  * @author  Mitchell Hoikka
+  * @version V1.2.0
+  * @date    10-9-2024
+  * @brief   Header for main.c module
+  ******************************************************************************
+  */
+
+void bme280_delay_microseconds(uint32_t usec, void *intf_ptr);
+int8_t BME280_I2C_bus_read(uint8_t reg_addr, uint8_t *reg_data, uint32_t cnt, void *intf_ptr);
+int8_t BME280_I2C_bus_write(uint8_t reg_addr, const uint8_t *reg_data, uint32_t cnt, void *intf_ptr);
+uint8_t* nrf24_read_register(uint8_t reg);
+void nrf24_write_register(uint8_t reg, uint8_t value);
+void test_nrf24_connection();
+void set_nrf24_SPI_CSN(uint8_t input);
+void set_nrf24_SPI_CE(uint8_t input);
+void MySPI_Init();
+void NRF24L01p_Init();
+void I2C_Settings_Init();
+void UART_Settings_Init();
--- src/main.h
+++ src/main.h
@@ -35,22 +35,10 @@
 /* Exported constants --------------------------------------------------------*/
 /* Exported macro ------------------------------------------------------------*/
 /* Exported functions ------------------------------------------------------- */
-int8_t BME280_I2C_bus_read(uint8_t reg_addr, uint8_t *reg_data, uint32_t cnt, void *intf_ptr);
-int8_t BME280_I2C_bus_write(uint8_t reg_addr, const uint8_t *reg_data, uint32_t cnt, void *intf_ptr);
-void bme280_delay_microseconds(uint32_t usec, void *intf_ptr);
 
 void TimingDelay_Decrement(void);
 void Delay(__IO uint32_t nTime);
 
-uint8_t* nrf24_read_register(uint8_t reg);
-void nrf24_write_register(uint8_t reg, uint8_t value);
-void test_nrf24_connection();
-void set_nrf24_SPI_CSN(uint8_t input);
-void set_nrf24_SPI_CE(uint8_t input);
-void MySPI_Init();
-void NRF24L01p_Init();
-void I2C_Settings_Init();
-void UART_Settings_Init();
 void System_Clock_Init();
 
 void display_sensor_reading();
