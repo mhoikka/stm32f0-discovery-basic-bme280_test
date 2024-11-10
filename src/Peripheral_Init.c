10e5a49f55c405b8755397eb2689a00cbb4d52a8
mhoikka <mhoikka@asu.edu>
2024-11-10 16:45:09 -0600

    Remove auto_ack

--- src/Peripheral_Init.c
+++ src/Peripheral_Init.c
@@ -341,13 +341,12 @@ void transmitByteNRF(uint8_t data){
     
     nrf24_write_register(CONFIG, 0x0A);         //set to PTX mode and turn on power bit 0x0A
     bme280_delay_microseconds(1.5*1000, NULL);  //wait for chip to go into Standby-I mode
+    nrf24_write_register(FEATURE, 0x01); //enable W_TX_PAYLOAD_NOACK command
 
     while(1){
-      nrf24_write_register(FEATURE, 0x01); //enable W_TX_PAYLOAD_NOACK command
-      nrf24_write_TX_payload(data, 1);            //write data to be transmitted into TX FIFO
       
+      nrf24_write_TX_payload(data, 0);            //write data to be transmitted into TX FIFO
       
-
       set_nrf24_SPI_CE(1);                  //enable chip to transmit data
       bme280_delay_microseconds(130, NULL); //wait for chip to go into TX mode
       Delay(1);
