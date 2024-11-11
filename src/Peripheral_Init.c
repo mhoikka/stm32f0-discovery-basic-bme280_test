9ae112538d26a9d48a36e9e4fdf666b89e2459d0
mhoikka <mhoikka@asu.edu>
2024-11-11 12:10:12 -0600

    Transmit more than 32 bytes

--- src/Peripheral_Init.c
+++ src/Peripheral_Init.c
@@ -381,7 +381,7 @@ void transmit(uint8_t * data, uint8_t data_len){
     //len_transmit = data_len > 32 ? 32 : data_len; //length of data to be transmitted this cycle
     len_left = data_len > 32 ? 32 : data_len; //delete this later
     memcpy(&data_seg[0], &data[i], len_left); //mini array of length 32 for buffering transmitted data
-    //memcpy(&data_send[0], &data_seg[0], len_left); //mini array of length 32 for buffering transmitted data
+    memcpy(&data_send[0], &data_seg[0], len_left); //mini array of length 32 for buffering transmitted data
 
     transmitBytesNRF(data_seg, len_transmit);
     //bme280_delay_microseconds(1000, NULL); //wait for transmission to complete
